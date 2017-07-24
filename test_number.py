b6ca6d332bb577861da98b527d68b350becf91f7
10hin <carl_friedrich_gauss_1777_1855@live.jp>
2017-07-25 07:16:15 +0900

    テストケース作成

--- test_number.py
+++ test_number.py
@@ -2,5 +2,8 @@ from number import Number
 import unittest
 
 class NumberTest(unittest.TestCase)
-    def isNumberTest_integerLiteral():
-        
+    def isNumberTest_integerLiteral(self):
+        self.assertTrue(Number.isnumber(1))
+
+if __name__ == '__main__':
+    unittest.main()
