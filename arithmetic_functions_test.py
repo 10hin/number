1e59faedfee0ef3e916b598f9a24a650b2e24cb6
10hin <10hin@outlook.jp>
2017-11-26 01:19:42 +0900

    テストスクリプトの修正と移動

--- arithmetic_functions_test.py
+++ arithmetic_functions_test.py
@@ -5,5 +5,5 @@ class GcdTests(unittest.TestCase):
     def gcdOfZero1():
         unittest.assertRaises(ArithmeticError, math.gcd, 0, 2)
 
-if __name__ == ‘__main__’:
+if __name__ == '__main__':
     unittest.main()
